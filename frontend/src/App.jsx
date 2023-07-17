7123f7db04a3a0eafe37d91421637ed0a58318ea
Maxim <toper.one111@gmail.com>
2023-07-17 22:33:36 +0300

    Create a Header component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,18 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
       <Router>
         <div className="container">
+          <Header />
           <Routes>
             <Route path="/" element={<Dashboard />} />
-            <Route path="/" element={<Login />} />
-            <Route path="/" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
           </Routes>
         </div>
       </Router>
