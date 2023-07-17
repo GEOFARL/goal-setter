0eeddf1837c7051dc8133b31acd601968734a5b2
Maxim <toper.one111@gmail.com>
2023-07-17 20:36:29 +0300

    Authenticate a user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -44,7 +44,20 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users/login
 // @access  Public
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({ message: 'Login User' });
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && user.matchPassword(password)) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid credentials');
+  }
 });
 
 // @desc    Get user data
